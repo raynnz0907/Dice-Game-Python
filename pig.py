b1a3864bc2df55d40bac9da5d04fcaaaf12599e3
raynnz0907 <rayan310807@gmail.com>
2026-04-11 19:44:30 +0530

    printing the winner

--- pig.py
+++ pig.py
@@ -44,3 +44,8 @@ while max(player_scores) < max_score:
 
         player_scores[player_index] += current_score
         print("Your total score is",player_scores[player_index])
+
+max_score = max(player_scores)
+winning_index = player_scores.index(max_score)
+print("Player",winning_index + 1, "is the winner !!")
+        
