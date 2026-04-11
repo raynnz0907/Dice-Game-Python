68f4c43ba1131c6af1fb97e6f5c7adc961efbc8c
raynnz0907 <rayan310807@gmail.com>
2026-04-11 19:42:26 +0530

    Player slection

--- pig.py
+++ pig.py
@@ -6,3 +6,17 @@ def roll():
     roll = random.randint(min_value , max_value )
 
     return roll    
+
+while True:
+    players = input("Enter the number of players(2-4): ")
+
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <=4:
+            break
+        else:
+            print("Must be between 2 - 4 players")
+
+    else:
+        print("invalid input, try agian.")
+
